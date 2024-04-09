bf0071e80f7e8e7b45c5496030675d1a02ec7906
Sergey D <themarquis@mail.ru>
2024-04-09 22:22:15 +0300

    сделал 4 задачу, потом понял что надо было просто вывести на экран и сделал 4* задачу

--- src/Main.java
+++ src/Main.java
@@ -29,5 +29,24 @@ public class Main {
         if (midPay % 1 == 0) {
             System.out.println("Средняя сумма трат за месяц составила " + (int) midPay + " рублей");
         } else System.out.println("Средняя сумма трат за месяц составила " + midPay + " рублей");
+
+        //Задача 4*
+        System.out.println("\nЗадача 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length; i > 0; i--) {
+            System.out.print(reverseFullName[i - 1]);
+        }
+
+        //Задача 4 со звёздочкой
+        System.out.println("\n\nЗадача 4 со звёздочкой");
+        char[] reverseArray = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 0; i < reverseArray.length / 2; i++) {
+            char temp = reverseArray[i];
+            reverseArray[i] = reverseArray[reverseArray.length - 1 - i];
+            reverseArray[reverseArray.length - 1 - i] = temp;
+        }
+        for (int i = 0; i < reverseArray.length; i++) {
+            System.out.print(reverseArray[i]);
+        }
     }
 }
